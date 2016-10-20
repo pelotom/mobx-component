544fcaca39a942d0c78f1271ada321b501a07e78
Tom Crockett <crockett@jpl.nasa.gov>
2016-10-20 16:32:17 -0700

    Remove build products from repo (but not from npm)

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-export default function <M>(Wrapped: React.ComponentClass<M> | React.SFC<M>): React.ComponentClass<{
-    model: M;
-}>;
